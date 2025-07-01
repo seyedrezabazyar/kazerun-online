f3d6c1e9d36b1590e55f4d6b30395de0cb6cf31e
seyedrezabazyar <seyedrezabazyar@gmail.com>
2025-07-01 14:47:48 +0330

    Install Laravel Breeze with admin seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@kazerun-online.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
